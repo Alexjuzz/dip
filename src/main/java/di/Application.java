07c397f1b676db86757a97b67cd760f4db6c1321
Alexjuzz <juzzle@mail.ru>
2024-03-16 02:45:12 +0300

    lst

--- src/main/java/di/Application.java
+++ src/main/java/di/Application.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
+q
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
